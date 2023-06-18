e4fef6c9281fff414784e30297a242a51f609dc3
Ha Trung Kien <trungkien04102002@gmail.com>
2023-06-18 18:05:54 +0700

    backup service

--- web/src/mirgations/teamMigration.ts
+++ web/src/mirgations/teamMigration.ts
@@ -6,7 +6,8 @@ import { TeamService } from "../services";
 export async function migrateTeam(app : WebApplication, folderName : string ) : Promise<void> {
     const dataFetchService : DataFetchService =  await app.get('services.DataFetchService');
     const teamService : TeamService =  await app.get('services.TeamService');
-    const url : string= 'http://localhost:5000/api/teams'
+    const url : string= 'https://footballapi.pulselive.com/football/compseasons/578/teams'
+    // const url : string = 'http://localhost:5000/api/teams'
     const teamData = await dataFetchService.fetchAndStore(folderName, 'teamsData.json', url)
     console.log('teamData when call syncTeamData function;',teamData)
     await teamService.synchTeamDb(teamData)
--- web/src/services/data-fetch.service.ts
+++ web/src/services/data-fetch.service.ts
@@ -1,7 +1,10 @@
+import { repository } from '@loopback/repository';
 import {injectable, /* inject, */ BindingScope, inject} from '@loopback/core';
 import { TeamService } from './team.service';
 import * as fs from 'fs';  
 import fetch from "node-fetch";
+const axios = require('axios');
+
 @injectable({scope: BindingScope.TRANSIENT})
 export class DataFetchService {
   constructor(
@@ -9,20 +12,48 @@ export class DataFetchService {
     public teamService : TeamService,
   ) {}
   public async fetchAndStore(folderName : string, fileName : string,url : string) : Promise<any>{
-    fs.mkdirSync(folderName);
-    const response = await fetch(url.toString());
-    const data = await response.json();
-    console.log('teamData when call fetchAndStore function;',data)
-    const folderPath = `storage/${folderName}`;
-
-    if (!fs.existsSync(folderPath)) {
-      fs.mkdirSync(folderPath, { recursive: true });
+    try {
+      const response = await axios.get('https://footballapi.pulselive.com/football/compseasons/578/teams', {
+        headers: {
+          'Origin': 'https://www.premierleague.com'
+        }
+      });
+      const data = response.data;
+      console.log(data)
     }
-
-    // Save to file 
-    fs.writeFileSync(`${folderPath}/${fileName.toString()}`, JSON.stringify(data));
-    console.log('Finish fetching data and saving file' + fileName.toString())
-    return data
+   
+    catch(error){
+      throw new Error('Fail to fetchAndStore')
+    }
   }
-
 }
+
+// @injectable({scope: BindingScope.TRANSIENT})
+// export class DataFetchService {
+//   constructor(
+//     @inject('services.TeamService')
+//     public teamService : TeamService,
+//   ) {}
+//   public async fetchAndStore(folderName : string, fileName : string,url : string) : Promise<any>{
+//     fs.mkdirSync(folderName);
+//     const response = await fetch(url, {
+//       headers: {
+//         'Origin': 'https://www.premierleague.com/'
+//       }
+//     });
+//     // const response = await fetch(url)
+//     // const data = await response.json();
+//     const data = response
+//     console.log('teamData when call fetchAndStore function;',data)
+//     const folderPath = `storage/${folderName}`;
+
+//     if (!fs.existsSync(folderPath)) {
+//       fs.mkdirSync(folderPath, { recursive: true });
+//     }
+
+//     // Save to file 
+//     fs.writeFileSync(`${folderPath}/${fileName.toString()}`, JSON.stringify(data));
+//     console.log('Finish fetching data and saving file' + fileName.toString())
+//     return data
+//   }
+// }
